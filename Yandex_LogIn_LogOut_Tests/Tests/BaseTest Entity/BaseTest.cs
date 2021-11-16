28758d9d47efffd2d495816ad562e95c5c5e7b49
Alexander Ulyanitsky <alex.backend.dev@gmail.com>
2021-11-16 16:12:03 +0300

    Added screenshot realization

--- Yandex_LogIn_LogOut_Tests/Tests/BaseTest Entity/BaseTest.cs
+++ Yandex_LogIn_LogOut_Tests/Tests/BaseTest Entity/BaseTest.cs
@@ -1,7 +1,9 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.IO;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 using Yandex_LogIn_LogOut_Tests.Page_Object_Entities;
@@ -28,6 +30,13 @@ namespace Yandex_LogIn_LogOut_Tests.Tests.BaseTest
         [TearDown]
         public void CloseDriver()
         {
+            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
+            {
+                var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+                string path = Path.Combine(Environment.CurrentDirectory, @"TestResults\", "Screenshoot.jpg");
+                screenshot.SaveAsFile(path, ScreenshotImageFormat.Jpeg);
+            }
+
             if (driver != null)
             {
                 driver.Quit();

f03f11c2b3d96516d24607984b9b50feb39deef5
Alexander Ulyanitsky <alex.backend.dev@gmail.com>
2021-11-21 23:15:28 +0300

    Added Selenoid realization

--- Yandex_LogIn_LogOut_Tests/Tests/BaseTest Entity/BaseTest.cs
+++ Yandex_LogIn_LogOut_Tests/Tests/BaseTest Entity/BaseTest.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
 using System.IO;
 using WebDriverManager;
@@ -17,10 +18,22 @@ namespace Yandex_LogIn_LogOut_Tests.Tests.BaseTest
         protected PassportYandexPage? passportYandexPage;
 
         [SetUp]
+        [Obsolete]
         public void Setup()
         {
             new DriverManager().SetUpDriver(new ChromeConfig());
-            driver = new ChromeDriver();
+            var driverOptions = new ChromeOptions();
+            var runName = GetType().Assembly.GetName().Name;
+            var timestamp = $"{DateTime.Now:yyyyMMdd.HHmm}";
+            driverOptions.AddAdditionalCapability("name", runName, true);
+            driverOptions.AddAdditionalCapability("videoName", $"{runName}.{timestamp}.mp4", true);
+            driverOptions.AddAdditionalCapability("logName", $"{runName}.{timestamp}.log", true);
+            driverOptions.AddAdditionalCapability("enableVNC", true, true);
+            driverOptions.AddAdditionalCapability("enableVideo", true, true);
+            driverOptions.AddAdditionalCapability("enableLog", true, true);
+            driverOptions.AddAdditionalCapability("screenResolution", "1920x1080x24", true);
+            driver = new RemoteWebDriver(new Uri("http://127.0.0.1:4444/wd/hub"), driverOptions);
+
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             yandexHomePage = new YandexHomePage(driver);
